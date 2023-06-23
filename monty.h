7c2ed9e3dcc7255b6ef5f6283bdeb83c806d77e3
Orateng <1818.blackmail@gmail.com>
2023-06-22 19:40:36 -0700

    My file

--- monty.h
+++ monty.h
@@ -63,7 +63,7 @@ void pop(stack_t **head, unsigned int counter);
 void swap(stack_t **head, unsigned int counter);
 void add(stack_t **head, unsigned int counter);
 void nop(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
+void sub(stack_t **head, unsigned int counter);
 void f_div(stack_t **head, unsigned int counter);
 void f_mul(stack_t **head, unsigned int counter);
 void f_mod(stack_t **head, unsigned int counter);
