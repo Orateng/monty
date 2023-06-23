38a9176621343d30985a0d7294051a992544f40b
Orateng <1818.blackmail@gmail.com>
2023-06-22 19:23:31 -0700

    My file

--- funcs.c
+++ funcs.c
@@ -12,7 +12,7 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 	instruction_t opst[] = {
 				{"push", push}, {"pall", pall},
 				{"pint",pint}, {"pop", pop},
-			       	{NULL, NULL}
+				{"swap", swap}, {NULL, NULL}
 				};
 	unsigned int i = 0;
 	char *op;
@@ -113,4 +113,35 @@ void addnode(stack_t **head, int n)
 	new_node->next = *head;
 	new_node->prev = NULL;
 	*head = new_node;
+}
+
+/**
+ * swap - adds the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void swap(stack_t **head, unsigned int counter)
+{
+        stack_t *h;
+        int len = 0, aux;
+
+        h = *head;
+        while (h)
+        {
+                h = h->next;
+                len++;
+        }
+        if (len < 2)
+        {
+                fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+                fclose(bus.file);
+                free(bus.content);
+                free_stack(*head);
+                exit(EXIT_FAILURE);
+        }
+        h = *head;
+        aux = h->n;
+        h->n = h->next->n;
+        h->next->n = aux;
 }
